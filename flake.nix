678f7f85ae933fa6f305d2732980a75d2a6bc1d1
Jared Baur <jaredbaur@fastmail.com>
2024-04-05 12:53:20 -0700

    Set nixfmt-rfc-style formatter

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "u-boot configuration within nix";
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }: {
+    formatter = nixpkgs.lib.mapAttrs (_: pkgs: pkgs.nixfmt-rfc-style) self.legacyPackages;
     overlays.default = import ./overlay.nix;
     checks = nixpkgs.lib.mapAttrs
       (_: pkgs: import ./checks.nix { inherit pkgs; })
