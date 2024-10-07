1fd7b9acb07acd92e86ead8c9adf4b53f08de6a4
Jared Baur <jaredbaur@fastmail.com>
2024-10-07 08:03:39 -0700

    Use recursive update with initial args

--- u-boot.nix
+++ u-boot.nix
@@ -62,88 +62,85 @@ let
 in
 stdenv.mkDerivation (
   finalAttrs:
-  (
-    initialArgs
-    // {
-      pname = "uboot-${boardName}";
-      version = "2024.10";
+  (lib.recursiveUpdate initialArgs {
+    pname = "uboot-${boardName}";
+    version = "2024.10";
 
-      src = fetchFromGitHub {
-        owner = "u-boot";
-        repo = "u-boot";
-        rev = "v${finalAttrs.version}";
-        hash = "sha256-UPy7XM1NGjbEt+pQr4oQrzD7wWWEtYDOPWTD+CNYMHs=";
-      };
+    src = fetchFromGitHub {
+      owner = "u-boot";
+      repo = "u-boot";
+      rev = "v${finalAttrs.version}";
+      hash = "sha256-UPy7XM1NGjbEt+pQr4oQrzD7wWWEtYDOPWTD+CNYMHs=";
+    };
 
-      postPatch = ''
-        patchShebangs tools scripts
+    postPatch = ''
+      patchShebangs tools scripts
+    '';
+
+    enableParallelBuilding = true;
+
+    depsBuildBuild = [ pkgsBuildBuild.stdenv.cc ];
+
+    nativeBuildInputs =
+      [
+        bc
+        bison
+        flex
+        gnutls
+        libuuid
+        ncurses
+        openssl
+        swig
+        which
+        xxd
+      ]
+      ++ (with python3Packages; [
+        libfdt
+        pyelftools
+        pyopenssl
+        setuptools
+      ]);
+
+    buildInputs = [ ];
+
+    makeFlags = [
+      "CROSS_COMPILE=${stdenv.cc.targetPrefix}"
+      "DTC=${lib.getExe' pkgsBuildBuild.dtc "dtc"}"
+    ] ++ extraMakeFlags;
+
+    inherit extraConfig;
+    passAsFile = [ "extraConfig" ];
+
+    configurePhase =
+      ''
+        runHook preConfigure
+      ''
+      + (
+        if configfile != null then
+          ''
+            install -Dm0644 ${configfile} .config
+          ''
+        else
+          ''
+            bash ${./merge_config.bash} configs/${boardName}_defconfig $extraConfigPath
+          ''
+      )
+      + ''
+        make olddefconfig
+        runHook postConfigure
       '';
 
-      enableParallelBuilding = true;
+    installPhase =
+      ''
+        runHook preInstall
+      ''
+      + (lib.concatMapStrings (file: ''
+        install -D --target-directory=$out ${file}
+      '') (artifacts ++ [ ".config" ]))
+      + ''
+        runHook postInstall
+      '';
 
-      depsBuildBuild = [ pkgsBuildBuild.stdenv.cc ];
-
-      nativeBuildInputs =
-        [
-          bc
-          bison
-          flex
-          gnutls
-          libuuid
-          ncurses
-          openssl
-          swig
-          which
-          xxd
-        ]
-        ++ (with python3Packages; [
-          libfdt
-          pyelftools
-          pyopenssl
-          setuptools
-        ]);
-
-      buildInputs = [ ];
-
-      makeFlags = [
-        "CROSS_COMPILE=${stdenv.cc.targetPrefix}"
-        "DTC=${lib.getExe' pkgsBuildBuild.dtc "dtc"}"
-      ] ++ extraMakeFlags;
-
-      inherit extraConfig;
-      passAsFile = [ "extraConfig" ];
-
-      configurePhase =
-        ''
-          runHook preConfigure
-        ''
-        + (
-          if configfile != null then
-            ''
-              install -Dm0644 ${configfile} .config
-            ''
-          else
-            ''
-              bash ${./merge_config.bash} configs/${boardName}_defconfig $extraConfigPath
-            ''
-        )
-        + ''
-          make olddefconfig
-          runHook postConfigure
-        '';
-
-      installPhase =
-        ''
-          runHook preInstall
-        ''
-        + (lib.concatMapStrings (file: ''
-          install -D --target-directory=$out ${file}
-        '') (artifacts ++ [ ".config" ]))
-        + ''
-          runHook postInstall
-        '';
-
-      meta.platforms = [ arch ];
-    }
-  )
+    meta.platforms = [ arch ];
+  })
 )
