df9d3e863636b848eb431ec61f1c5ec9e16c1ba9
Jared Baur <jaredbaur@fastmail.com>
2025-06-23 11:08:56 -0700

    Update imx firmware

--- misc/imx-firmware.nix
+++ misc/imx-firmware.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "imx-firmware";
-  version = "8.20";
+  version = "8.22";
 
   src = fetchurl {
     url = "http://sources.buildroot.net/firmware-imx/firmware-imx-${finalAttrs.version}.bin";
-    hash = "sha256-9txqXI/ZuROhU2DTzNU9GI2wWgioWUxRjldiJHjHI4M=";
+    hash = "sha256-lMi86sVuxQPCMuYU931rvY4Xx9qnHU5lHqj9UDTDA1A=";
   };
 
   dontUnpack = true;
@@ -18,9 +18,11 @@ stdenv.mkDerivation (finalAttrs: {
   dontFixup = true;
 
   installPhase = ''
+    runHook preInstall
     bash $src --auto-accept --force
     find firmware-imx-${finalAttrs.version}/firmware -type f \
       -exec install -D --target-directory=$out {} \;
+    runHook postInstall
   '';
 
   meta.license = lib.licenses.unfree;
