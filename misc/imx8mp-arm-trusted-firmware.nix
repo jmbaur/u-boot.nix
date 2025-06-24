7e1f14a76eb5d0c8a620cbf44cfdb6a55d75d452
Jared Baur <jaredbaur@fastmail.com>
2025-06-24 14:08:26 -0700

    Add more ATF builds

--- misc/imx8mp-arm-trusted-firmware.nix
+++ misc/imx8mp-arm-trusted-firmware.nix
@@ -1,6 +1,6 @@
 { buildArmTrustedFirmware }:
 buildArmTrustedFirmware rec {
-  platform = "imx8mm";
+  platform = "imx8mp";
   extraMeta.platforms = [ "aarch64-linux" ];
   extraMakeFlags = [ "IMX_BOOT_UART_BASE=auto" ];
   filesToInstall = [ "build/${platform}/release/bl31.bin" ];
--- overlay.nix
+++ overlay.nix
@@ -2,5 +2,6 @@ final: prev:
 {
   imxFirmware = prev.callPackage ./misc/imx-firmware.nix { };
   armTrustedFirmwareImx8mm = prev.callPackage ./misc/imx8mm-arm-trusted-firmware.nix { };
+  armTrustedFirmwareImx8mp = prev.callPackage ./misc/imx8mp-arm-trusted-firmware.nix { };
 }
 // (import ./boards.nix { pkgs = final; })
