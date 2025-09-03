226b664f2dd400c713833ad851b9682aa5dbec56
Jared Baur <jaredbaur@fastmail.com>
2025-09-03 22:17:58 +0100

    Update to use newer buildArmTrustedFirmware

--- misc/imx8mm-arm-trusted-firmware.nix
+++ misc/imx8mm-arm-trusted-firmware.nix
@@ -1,7 +1,7 @@
 { buildArmTrustedFirmware }:
-buildArmTrustedFirmware rec {
+buildArmTrustedFirmware (finalAttrs: {
   platform = "imx8mm";
-  extraMeta.platforms = [ "aarch64-linux" ];
-  extraMakeFlags = [ "IMX_BOOT_UART_BASE=auto" ];
-  filesToInstall = [ "build/${platform}/release/bl31.bin" ];
-}
+  meta.platforms = [ "aarch64-linux" ];
+  makeFlags = [ "IMX_BOOT_UART_BASE=auto" ];
+  filesToInstall = [ "build/${finalAttrs.platform}/release/bl31.bin" ];
+})
